a59f48463c440f5f62c996c3ad032d0cfec96ab1
Joel Håkansson <joel.hakansson@mtm.se>
2018-01-11 08:55:21 +0100

    Creates a factory for previewers and supports html using it

---
+++ test/org/daisy/dotify/studio/api/EditorMakerTest.java
@@ -0,0 +1,12 @@
+package org.daisy.dotify.studio.api;
+
+import org.junit.Test;
+
+public class EditorMakerTest {
+
+	@Test
+	public void testMaker() {
+		//FIXME:
+		PreviewMaker.newInstance().supportsFormat(null);
+	}
+}
